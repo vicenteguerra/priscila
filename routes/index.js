151c33f2e8ca247b8fb3745d22e88193dff847d6
Vicente Guerra <vicenteguerrahdez@gmail.com>
2018-05-21 16:29:27 -0500

    MainController file

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 var express = require('express');
-var router = express.Router();
-
+let router = express.Router();
+let Priscila = require('../controllers/Core/PriscilaController').Priscila;
 
 let User = require('../controllers/API/UserController').User;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Voice Payment' });
+  res.render('index', { title: 'Priscila' });
 });
+
+router.get('/message', Priscila.processMessage);
 
 router.get('/user', User.index);
 router.get('/user/:id', User.show);
 router.post('/user', User.create);
 router.delete('/user/:id', User.destroy);
 router.patch('/user/:id', User.update);
+
 
 module.exports = router;
