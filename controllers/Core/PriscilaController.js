f3bea1bb124a898bb84fc733e587baac2d47e5a0
Vicente Guerra <vicenteguerrahdez@gmail.com>
2018-05-21 17:23:53 -0500

    Find Products

--- controllers/Core/PriscilaController.js
+++ controllers/Core/PriscilaController.js
@@ -1,6 +1,7 @@
 let Models = require('../../models/index');
 let SMS = require('../Utils/SMSController').SMS;
 let Payment = require('../API/PaymentController').Payment;
+const Op = Models.Sequelize.Op;
 
 let Priscila = function () {
     this.processMessage = processMessage;
@@ -9,7 +10,7 @@ let Priscila = function () {
 let processMessage = function (req, res) {
     let data = req.body;
 
-    if(!data.user_id){
+    if(!data.user_id || !data.products){
         return res.json({
             success: false,
             response:  "User not found"
@@ -27,19 +28,49 @@ let processMessage = function (req, res) {
                 response:  "User not found"
             });
         }
-        let sms_data = {
-            phone: user.get("phone")
-        };
-        Payment.creaCadenaPago({
-            reference: "Cafeteria MIT",
-            amount: 45
-        }).then(function (response) {
-            sms_data.message = response.nb_url;
-            SMS.send(sms_data);
-            return res.json({
-                success: true,
-                url: response.nb_url
-            })
+
+        let array_names = data.products.map(function (product) {
+            return {name: product.name};
+        });
+
+
+        Models.Product.findAll({
+            where: {
+                [Op.or] : array_names
+            }
+        }).then(function (products) {
+            console.log(JSON.stringify(products));
+            var total = 0;
+            products.forEach(function (product_db) {
+                var product = data.products.find(function (product) {
+                    if(product.name == product_db.get("name")){
+                        return product;
+                    }
+                })
+                if(product){
+                    total += parseInt(product.quantity) * product_db.get("price");
+                }
+            });
+            let sms_data = {
+                phone: user.get("phone")
+            };
+            if(total <= 0){
+                return res.json({
+                    success: false,
+                    response:  "Amount not valid"
+                });
+            }
+            Payment.creaCadenaPago({
+                reference: "Cafeteria MIT",
+                amount: total
+            }).then(function (response) {
+                sms_data.message = response.nb_url;
+                SMS.send(sms_data);
+                return res.json({
+                    success: true,
+                    url: response.nb_url
+                })
+            });
         });
     });
 };
